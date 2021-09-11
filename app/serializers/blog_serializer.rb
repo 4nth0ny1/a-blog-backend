4cbfa7683a0bf912f2c3bcf31bfcad6f710e257e
4nth0ny1 <acatullo4@gmail.com>
2021-09-11 13:02:34 -0500

    add serializers for formated created at and last update times

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,13 @@
 class BlogSerializer < ActiveModel::Serializer
   has_many :comments
   belongs_to :subject
-  attributes :id, :title, :content, :signature, :rank, :subject_id
+  attributes :id, :title, :content, :signature, :rank, :subject_id, :formatted_created_at, :last_updated_at
+
+  def formatted_created_at 
+    Time.use_zone('America/Chicago') {object.created_at.strftime("%m/%d/%Y, %I:%M%p")}
+  end 
+
+  def last_updated_at 
+    Time.use_zone('America/Chicago') {object.updated_at.strftime("%m/%d/%Y, %I:%M%p")}
+  end 
 end
